4eb43a31434f9fb5cfbe64a3a1a18db08d4e39a3
AllanyG <allanygabrielly16@gmail.com>
2025-12-02 11:51:34 -0300

    Atualizando main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
+// Importa polyfills para compatibilidade com navegadores
 import './polyfills';
 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+// Importa o módulo raiz da aplicação
 import { AppModule } from './app/app.module';
 
+// Inicializa a aplicação Angular carregando o AppModule
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
+    // Destrói instância anterior se existir
   if (window['ngRef']) {
     window['ngRef'].destroy();
   }
   window['ngRef'] = ref;
 
-  // Otherwise, log the boot error
+// Tratamento de erro durante a inicialização
 }).catch(err => console.error(err));
