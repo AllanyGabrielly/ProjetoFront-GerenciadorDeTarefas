8e3d6de4391e555e0d9bd2b279d55ed0a9aecf0f
AllanyG <allanygabrielly16@gmail.com>
2025-12-02 10:36:42 -0300

    Update AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+// Definição do componente raiz da aplicação Angular
 import { Component } from '@angular/core';
 
+// Configuração do componente com seletor, template e estilos
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// Classe do componente principal da aplicação
 export class AppComponent {
   title = 'Gerenciador de Tarefas';
 }
