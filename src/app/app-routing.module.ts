acf95c25f9c3425defda45c857209c028738fde6
AllanyG <allanygabrielly16@gmail.com>
2025-12-02 10:30:52 -0300

    Editando o arquivo app-routing.module.ts para maior clareza

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
+// Importação dos módulos e componentes necessários
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaTarefasComponent } from './components/lista-tarefas/lista-tarefas.component';
 import { FormularioTarefaComponent } from './components/formulario-tarefa/formulario-tarefa.component';
 
+// Definição das rotas da aplicação
 export const routes: Routes = [
   { path: '', component: ListaTarefasComponent },
   { path: 'nova', component: FormularioTarefaComponent },
@@ -10,8 +12,10 @@ export const routes: Routes = [
   { path: '**', redirectTo: '' }
 ];
 
+// Configuração do módulo de roteamento principal
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
