5dd6cce0c385e0761e663416fe65428010f48531
AllanyG <allanygabrielly16@gmail.com>
2025-12-02 10:49:06 -0300

    Aprimorando FormularioTarefaComponent
    Refactor component code with added comments for clarity.

--- src/app/components/formulario-tarefa/formulario-tarefa.component.ts
+++ src/app/components/formulario-tarefa/formulario-tarefa.component.ts
@@ -1,12 +1,15 @@
+// Componente do formulário para criação-edição de tarefas com comunicação via Input-Output
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Tarefa } from '../../models/tarefa.interface';
 
+// Configuração do componente com template e estilos
 @Component({
   selector: 'app-formulario-tarefa',
   templateUrl: './formulario-tarefa.component.html',
   styleUrls: ['./formulario-tarefa.component.css']
 })
+// Classe do componente com inicialização de formulário reativo
 export class FormularioTarefaComponent implements OnInit {
   @Input() tarefa?: Tarefa;
   @Output() salvar = new EventEmitter<Omit<Tarefa, 'id'>>();
@@ -17,6 +20,7 @@ export class FormularioTarefaComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {}
 
+ // Inicialização do formulário com validações e dados da tarefa (se for edição)
   ngOnInit(): void {
     this.modoEdicao = !!this.tarefa;
     
@@ -32,7 +36,8 @@ export class FormularioTarefaComponent implements OnInit {
       ]
     });
   }
-
+  
+ // Método para envio do formulário com validação e emissão de dados
   onSubmit(): void {
     if (this.formulario.valid) {
       const dadosTarefa = {
@@ -44,8 +49,9 @@ export class FormularioTarefaComponent implements OnInit {
       this.salvar.emit(dadosTarefa);
     }
   }
-
+ // Método para cancelar a operação (criação/edição)
   onCancelar(): void {
     this.cancelar.emit();
   }
+
 }
